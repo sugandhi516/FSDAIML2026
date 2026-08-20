0baf8f4d98e5c06dcd9b4cce3c1921cb6d5c3592
sugandhi516 <bansalsugandhi6@gmail.com>
2026-08-20 10:17:36 +0530

    20th August

--- useFetch.js
+++ useFetch.js
@@ -1,12 +1,71 @@
-const div = document.getElementById("container");
-const button = document.getElementById("btn");
-async function display() {
-    const serverdata =await fetch("https://fakestoreapi.com/products");
-   const jsondata = await serverdata.json();
-   console.log(jsondata);
-   console.log(jsondata[0].title);
-   console.log(jsondata[1].price);
-   // div.innerHTML= '<h1>Hello using DOM</h1>';
+const div = document.getElementById('container');
+const button = document.getElementById('btn');
 
+async function display() {
+
+    const h2 = document.createElement('h2');
+    h2.innerText = 'Data is loading...';
+
+    try {
+        div.appendChild(h2);
+
+        const serverdata = await fetch('https://fakestoreapi.com/products');
+        const jsonData = await serverdata.json();
+
+        let table = `
+            <table border="1" cellspacing="0" cellpadding="10">
+                <tr>
+                    <th>Image</th>
+                    <th>Id</th>
+                    <th>Item-title</th>
+                    <th>Price</th>
+                    <th>Action</th>
+                </tr>
+
+                ${jsonData.map((ele) => {
+                    return `
+                        <tr>
+                            <td>
+                                <img src="${ele.image}" 
+                                     width="80" 
+                                     height="80">
+                            </td>
+
+                            <td>${ele.id}</td>
+
+                            <td>${ele.title}</td>
+
+                            <td>$${ele.price}</td>
+
+                            <td>
+                                <button onclick="addToCart(${ele.id})">
+                                    Add to Cart
+                                </button>
+                            </td>
+                        </tr>
+                    `;
+                }).join('')}
+
+            </table>
+        `;
+
+        div.innerHTML = table;
+
+    } catch (error) {
+
+        console.log(error);
+        div.innerHTML = `<h2>Error loading data</h2>`;
+
+    } finally {
+
+        h2.remove();
+    }
 }
-button.addEventListener("click", display); 
+
+
+function addToCart(id) {
+    console.log("Product added to cart:", id);
+}
+
+
+button.addEventListener('click', display);
