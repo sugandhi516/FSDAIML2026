e27be7ade7250414856f835ea7a7a5959edb28c1
sugandhi516 <bansalsugandhi6@gmail.com>
2026-09-10 10:27:15 +0530

    Create React ID card

--- reactvite/src/App.jsx
+++ reactvite/src/App.jsx
@@ -1,32 +1,11 @@
-
-import ICard from './component/ICard'
-import './App.css'
+import Card from "./component/ICard";
 
 function App() {
-  let a = 23;
-  let name = "Ram";
-
-  return (
-    <div
-      style={{
-        border: "1px solid black",
-        width: "1000px",
-        height: "400px",
-        textAlign: "center"
-      }}
-    >
-      <h2>Welcome to React using Vite</h2>
-
-      <h2>Value of a={a}</h2>
-
-      <h2>Name={name}</h2>
-
-      <div>
-        <ICard />
-      </div>
-
-    </div>
-  )
+    return (
+        <div>
+            <Card />
+        </div>
+    );
 }
 
-export default App
+export default App;
--- reactvite/src/component/ICard.jsx
+++ reactvite/src/component/ICard.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
-function ICard() {
-  return (
-    <div style={{border: "4px solid red", width: "1000px", height: "100px"}}>
-        <h1 style={{color: 'white', backgroundColor: 'brown'}}>ABES Engineering College</h1>
-    </div>
-  )
-}   
-export default ICard
+function Card() {
+    return (
+        <div
+            style={{
+                border: "10px solid brown",
+                height: "400px",
+                width: "300px",
+                borderRadius: "15px",
+                textAlign: "center",
+                margin: "50px auto",
+                backgroundColor: "white"
+            }}
+        >
+            <h2
+                style={{
+                    backgroundColor: "brown",
+                    color: "black",
+                    padding: "15px",
+                    margin: "0"
+                }}
+            >
+                ABES Engineering College
+            </h2>
+            <h2
+                style={{
+                    color: "black",
+                    marginTop: "30px"
+                }}
+            >
+                Sugandhi Bansal
+            </h2>
+            <img src="/passport size photo.jpg"alt="Sugandhi Bansal"style={{ width: "100px",height: "100px",  borderRadius: "50%",objectFit: "cover",display: "block",margin: "20px auto" }}/>
+
+            <p style={{ color: "black" }}>Roll No: 2400321530198</p>
+
+            <p style={{ color: "black" }}>Branch: CSE - AIML</p>
+
+            <p style={{ color: "black" }}>College: ABES Engineering College</p>
+            <p style={{ color: "black" }}>Section: C</p>
+            
+        </div>
+    );
+}
+export default Card;
